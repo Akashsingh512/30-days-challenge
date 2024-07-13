538a7abc696bfa2bba410ee35fb3edca527174e2
Aakash Singh <93008632+Akashsingh512@users.noreply.github.com>
2024-07-13 03:35:51 +0000

    Day One challenge Changes

---
+++ 01_Day_Task/01_day.js
@@ -0,0 +1,30 @@
+var number = 5; 
+let string = "Its a string";
+const Bool1 = true;
+
+console.log(number)
+console.log(string)
+console.log(Bool1)
+
+var Object = [1,2,3,4];
+console.log(Object)
+
+var Array1 = [1,2,3,4]
+console.log(Array1);
+
+console.log(typeof(Object));
+console.log(typeof(Array1));
+
+let TwoValue = 5;
+console.log(TwoValue);
+
+TwoValue = 10;
+
+console.log(TwoValue);
+
+const TwoValue2 = 6;
+console.log(TwoValue2);
+
+// TwoValue2 = 8;
+// console.log(TwoValue2);
+
---
+++ 01_Day_Task/README.md
@@ -0,0 +1,13 @@
+# 30-days-challenge
+
+## Day One Challenge 
+
+- Day one challenge is easy but it teach a alot of things about.
+
+    - Variables
+    - type of 
+    - Difference between var and const
+    - Error Handling
+
+
+### Thank You
